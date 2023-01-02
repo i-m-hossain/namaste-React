8a9e4cbac21e34388308e97adf739a6a328a21f9
imranhAltersense <114974565+imranhAltersense@users.noreply.github.com>
2023-01-02 08:30:12 +0600

    parcel bundler added (iginiting our app)

--- app.js
+++ app.js
@@ -1,18 +1,10 @@
-/* In js way 
+import React from "react";
+import ReactDOM  from "react-dom/client ";
 
-const heading = document.createElement("h1");
-heading.textContent = "hello everyone";
-const rootDom = document.getElementById("root");
-rootDom.appendChild(heading); 
-
-*/
-
-/*--------- IN REACT WAY------- */
-
-const h1 = React.createElement("h1", {title:"h1"}, "Namaste react ");
-const h2 = React.createElement("h2", {id:"h2"}, "Namaste react");
-const h3 = React.createElement("h3", {class:"h3"}, "Namaste react");
-const div = React.createElement("div", {id: "container"}, [h1, h2, h3]);
+const h1 = React.createElement("h1", { title: "h1" }, "Namaste react ");
+const h2 = React.createElement("h2", { id: "h2" }, "Namaste react");
+const h3 = React.createElement("h3", { className: "h3" }, "Namaste react");
+const div = React.createElement("div", { id: "container" }, [h1, h2, h3]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(div);
