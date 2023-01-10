f5d4602f91e9d3c29bc70fe3c802922b34441eda
imranhAltersense <114974565+imranhAltersense@users.noreply.github.com>
2023-01-10 23:58:54 +0600

    lesson 3 completed

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client ";
 
-const h1 = React.createElement("h1", { title: "h1" }, "Namaste react ");
-const h2 = React.createElement("h2", { id: "h2" }, "Namaste react");
-const h3 = React.createElement("h3", { className: "h3" }, "Namaste react");
-// const div = React.createElement("div", { id: "container" }, [h1, h2, h3]);
-const  container = <div>
-    <h1>Namaste react 1</h1>
-    <h1>Namaste react 2</h1>
-    <h1>Namaste react 3</h1>
-
-</div>
+const Title = () => <h1 id="title" key="title1">Namaste React</h1>;
+const HeaderComponent = () => (
+  <div>
+    <Title />
+    <h2> Namaste React functional component</h2>
+    <h2> this is h2 tag</h2>
+  </div>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+root.render(<HeaderComponent />);
